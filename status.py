a7fc6b44ef667b73dbf0beef083d92c312a953b2
Jacopo <jacopo.carlini@gmail.com>
2023-10-17 17:23:04 +0200

    workflow

--- status.py
+++ status.py
@@ -1,13 +1,16 @@
-import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/status/live', methods=['GET'])
 def get_live():
-    return jsonify({"status":"ok"})
+    return jsonify({"status": "ok"})
+
+
 @app.route('/status/ready', methods=['GET'])
 def get_ready():
-    return jsonify({"status":"ok"})
+    return jsonify({"status": "ok"})
+
 
 app.run(port=8080)
