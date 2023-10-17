56cf69d56b26e0ac0a5936a21a0681a35172ccea
Jacopo <jacopo.carlini@gmail.com>
2023-10-17 18:41:17 +0200

    fix

--- scripts/connect_oracle.py
+++ scripts/connect_oracle.py
@@ -18,8 +18,8 @@ print("Cursor created")
 print("Executing query...")
 sqlQuery = str("""
                     UPDATE NODO4_CFG.PSP
-                    SET  NODO4_CFG.PSP='ABC'
-                    WHERE NODO4_CFG.PSP.ID_PSP = 'JACOPO'
+                    SET  PSP='ABC'
+                    WHERE PSP.ID_PSP = 'JACOPO'
                    """)
 
 cursor.execute(sqlQuery)
