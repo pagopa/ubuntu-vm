a626682255f2014d2a2f7a6f51a1d60f3b03d3a7
Jacopo <jacopo.carlini@gmail.com>
2023-10-25 12:46:00 +0200

    script python

--- scripts/connect_oracle.py
+++ scripts/connect_oracle.py
@@ -1,6 +1,7 @@
 import os
-
+import csv
 import oracledb
+
 
 connection = oracledb.connect(
     dsn="db-nodo-pagamenti.d.db-nodo-pagamenti.com/NDPSPCT_PP_NODO4_CFG",
@@ -8,21 +9,29 @@ connection = oracledb.connect(
     user=os.environ['SPRING_DATASOURCE_USERNAME'],
     password=os.environ['SPRING_DATASOURCE_PASSWORD']
 )
-
 print("Successfully connected to Oracle Database")
 
-print("Creating cursor...")
-cursor = connection.cursor()
-print("Cursor created")
 
-print("Executing query...")
-sqlQuery = str("""
-                    UPDATE NODO4_CFG.PSP
-                    SET  PSP='ABC'
-                    WHERE PSP.ID_PSP = 'JACOPO'
-                   """)
+with open('psp.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    values = []
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t PSP {row[1]} set taxcode {row[2]}')
+            line_count += 1
+            values.append((row[2], row[1]))
 
-cursor.execute(sqlQuery)
+    print("Executing query...")
+    cursor = connection.cursor()
+    cursor.prepare("UPDATE NODO4_CFG.PSP  SET PSP.CODICE_FISCALE= :1  WHERE PSP.ID_PSP = :2")
+    cursor.executemany(None, values)
+    connection.commit()
+
+    print(f'Processed {line_count} lines.')
+
 connection.close()
-
 print("Done")
